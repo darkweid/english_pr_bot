f0d1328fed025f1f3dcc42dbcbc2a31d7b506d55
Mark Shamanskii <darkweid713@gmail.com>
2023-09-02 10:16:58 +0530

    добавлены тестовые словари

---
+++ files/words.py
@@ -0,0 +1,129 @@
+family_vocabulary = {
+    "father": "отец",
+    "mother": "мать",
+    "brother": "брат",
+    "sister": "сестра",
+    "grandfather": "дедушка",
+    "grandmother": "бабушка",
+    "son": "сын",
+    "daughter": "дочь",
+    "uncle": "дядя",
+    "aunt": "тётя"
+}
+
+food_vocabulary = {
+    "apple": "яблоко",
+    "banana": "банан",
+    "bread": "хлеб",
+    "milk": "молоко",
+    "cheese": "сыр",
+    "rice": "рис",
+    "egg": "яйцо",
+    "chicken": "курица",
+    "fish": "рыба",
+    "vegetable": "овощ"
+}
+
+numbers_vocabulary = {
+    "one": "один",
+    "two": "два",
+    "three": "три",
+    "four": "четыре",
+    "five": "пять",
+    "six": "шесть",
+    "seven": "семь",
+    "eight": "восемь",
+    "nine": "девять",
+    "ten": "десять"
+}
+
+animals_vocabulary = {
+    "dog": "собака",
+    "cat": "кот",
+    "horse": "лошадь",
+    "rabbit": "кролик",
+    "elephant": "слон",
+    "lion": "лев",
+    "tiger": "тигр",
+    "monkey": "обезьяна",
+    "bird": "птица",
+    "fish": "рыба"
+}
+
+colors_vocabulary = {
+    "red": "красный",
+    "blue": "синий",
+    "green": "зелёный",
+    "yellow": "желтый",
+    "orange": "оранжевый",
+    "purple": "пурпурный",
+    "pink": "розовый",
+    "brown": "коричневый",
+    "white": "белый",
+    "black": "черный"
+}
+
+clothing_vocabulary = {
+    "shirt": "рубашка",
+    "pants": "брюки",
+    "dress": "платье",
+    "shoes": "туфли",
+    "hat": "шляпа",
+    "socks": "носки",
+    "jacket": "куртка",
+    "skirt": "юбка",
+    "gloves": "перчатки",
+    "scarf": "шарф"
+}
+
+weather_vocabulary = {
+    "sun": "солнце",
+    "rain": "дождь",
+    "clouds": "облака",
+    "wind": "ветер",
+    "snow": "снег",
+    "storm": "шторм",
+    "temperature": "температура",
+    "umbrella": "зонт",
+    "thunder": "гроза",
+    "fog": "туман"
+}
+
+transport_vocabulary = {
+    "car": "автомобиль",
+    "bus": "автобус",
+    "bicycle": "велосипед",
+    "train": "поезд",
+    "plane": "самолет",
+    "boat": "лодка",
+    "subway": "метро",
+    "motorcycle": "мотоцикл",
+    "taxi": "такси",
+    "tram": "трамвай"
+}
+
+school_vocabulary = {
+    "teacher": "учитель",
+    "student": "ученик",
+    "book": "книга",
+    "pen": "ручка",
+    "desk": "парта",
+    "board": "доска",
+    "classroom": "класс",
+    "homework": "домашнее задание",
+    "test": "тест",
+    "backpack": "рюкзак"
+}
+
+school_subjects = {
+    "math": "математика",
+    "science": "наука",
+    "history": "история",
+    "geography": "география",
+    "language": "язык",
+    "art": "искусство",
+    "music": "музыка",
+    "physical education": "физическая культура",
+    "chemistry": "химия",
+    "computer science": "информатика"
+}
